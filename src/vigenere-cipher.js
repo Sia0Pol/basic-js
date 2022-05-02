19489959983c40a62db8946f7798abb7e2d9f7b3
sia0pol <nassypolish@gmail.com>
2022-05-02 21:32:36 +0700

    feat: fix

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw Error("Incorrect arguments!");
+    }
+
+    let result = [], l = 0;
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    
+    for (let element of message) {
+      if (element.charCodeAt(0) >= 65 && element.charCodeAt(0) <= 90) {
+        element = String.fromCharCode((element.charCodeAt(0) - 65 + key[l++ % key.length].charCodeAt(0) - 65) % 26 + 65);
+      }
+
+      result.push(element);
+    }
+    return (this.isDirect ? result : result.reverse()).join("");
+  }
+
+  decrypt(message, key) {
+  if (!message || !key) throw Error("Incorrect arguments!");
+    let result = [], l = 0;
+
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let element of message) {
+      if (element.charCodeAt(0) >= 65 && element.charCodeAt(0) <= 90) {
+        element = String.fromCharCode((element.charCodeAt(0) + 65 - key[l++ % key.length].charCodeAt(0) + 65) % 26 + 65);
+      }
+
+      result.push(element);
+    }
+    return (this.isDirect ? result : result.reverse()).join("");
   }
 }
 
